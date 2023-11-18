6a9cc9e8de18d0108b1e79b1b0fd7d31a9be08dc
Amirhosein <148243959+plutuxx@users.noreply.github.com>
2023-11-18 20:40:59 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let displayStatus = false;
+const allAnswers = document.getElementsByClassName('answers');
+const allQuestions = document.getElementsByClassName('questions');
+const allArrow = document.getElementsByClassName('arrow')
+const illustrationBox = document.getElementById('illustration-box')
 
+let displayStatus = false;
 let id = 'answerId'
 
 //Function for showing and hiding the answer
@@ -10,24 +14,25 @@ function answerer(questionBox, answerId, theAnswer) {
 
     // For showing the answer
     if (displayStatus == false) {
+        
         displayStatus = true;
         question.style.fontWeight = 900;
         answer.innerHTML += `<p id="answer" class="answers">${theAnswer}</p>`;
         answer.style.marginTop = '15px';
         arrow.style.rotate = '180deg'
+        illustrationBox.style.left = '-5%'
     } // For hiding the answer
     else if (displayStatus == true && answerId === id) {
+        
         displayStatus = false;
         question.style.fontWeight = '';
         answer.style.marginTop = '';
         answer.querySelector('p').remove();
         arrow.style.rotate = ''
-
+        illustrationBox.style.left = ''
     } // For showing the desired answer and hiding the rest of the answers
     else if (displayStatus == true) {
-        const allAnswers = document.getElementsByClassName('answers');
-        const allQuestions = document.getElementsByClassName('questions');
-        const allArrow = document.getElementsByClassName('arrow')
+
         //hiding all answers
         document.getElementById(`${id}`).style.marginTop = '';
         allAnswers[0].remove()
@@ -48,7 +53,6 @@ function answerer(questionBox, answerId, theAnswer) {
 function pageSizeCheck() {
     const bgPattern = document.getElementById('bg-pattern')
     const illustrationWoman = document.getElementById('illustration-woman')
-    var illustrationBox = document.getElementById('illustration-box')
     if (window.innerWidth <= 767) {
         bgPattern.setAttribute('src', 'images/bg-pattern-mobile.svg')
         illustrationWoman.setAttribute('src', 'images/illustration-woman-online-mobile.svg')
